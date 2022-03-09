ca3c1cad97d795f1920ad84374148c098d4b5764
Gabriel Alves <alvezinho.gabs@gmail.com>
2022-03-09 20:04:19 -0300

    Melhorando o código

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,25 @@
 
+
+let btn = document.getElementById('menu-btn');
+btn.addEventListener('click', toggleMenu)
+
 function toggleMenu(){
     let nav = document.getElementById('nav');
     nav.classList.toggle('active');
 }
 
-let btn = document.getElementById('menu-btn');
-btn.addEventListener('click', toggleMenu)
+
+
+
+
+if (window.matchMedia('(min-width: 855px)').matches) {
+    let cabeca = document.getElementsByTagName('header')[0];
+    let cafe = document.getElementById('cafe');
+    
+    cabeca.appendChild(cafe);
+} else{
+    let principal = document.getElementsById('container__cafe')
+    let cafe = document.getElementById('cafe');
+
+    principal.appendChild(cafe);
+}
