924351d979ab06ca4b237e0bf4ed27da92ec249b
Gabriel Alves <alvezinho.gabs@gmail.com>
2022-03-10 10:25:54 -0300

    Ajustes

--- scripts/script.js
+++ scripts/script.js
@@ -1,26 +1,11 @@
 
 
 let btn = document.getElementById('menu-btn');
-btn.addEventListener('click', toggleMenu);
+btn.addEventListener('click', toggleMenu);  //serve para acionar a função quando houver um click no botão selecionado, no caso o botão de menu do header
 
-function toggleMenu(){
-    let nav = document.getElementById('nav');
-    nav.classList.toggle('active');
+function toggleMenu(){                  //Função utilizada no menu responsivo;
+    let nav = document.getElementById('nav');       
+    nav.classList.toggle('active'); //adicionar uma classe ao nav;
 }
 
 
-
-
-/*
-if (window.matchMedia('(min-width: 855px)').matches) {
-    let cabeca = document.getElementsByTagName('header')[0];
-    let cafe = document.getElementById('cafe');
-    
-    cabeca.appendChild(cafe);
-} else{
-    let principal = document.getElementsById('container__cafe')
-    let cafe = document.getElementById('cafe');
-
-    principal.appendChild(cafe);
-}
-*/
